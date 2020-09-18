77efced3d57122533034f3443778eab0f9c25804
Gus Heptig <gusheptig@gmail.com>
2020-09-18 15:32:52 -0400

    still trying to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./models");
 const routes = require("./routes");
@@ -8,13 +9,11 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the Vue app build directory
-app.use(express.static(path.join(__dirname, "client/dist")));
-
 // Express boilerplate middleware
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routing
 // =============================================
@@ -22,6 +21,13 @@ app.use("/api", routes);
 
 // Everything that is not an api request is sent to index.html
 // for client side routing.
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public/"));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
