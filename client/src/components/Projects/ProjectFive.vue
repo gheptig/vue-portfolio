1da761374975d793db915ff55148661f7160deea
Gus Heptig <gusheptig@gmail.com>
2020-09-29 22:37:15 -0400

    updated projects

--- client/src/components/Projects/ProjectFive.vue
+++ client/src/components/Projects/ProjectFive.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="row justify-content-center no-gutters">
     <div class="col-lg-6">
-      <a href="https://gheptig.github.io/timed-quiz/" target="_blank">
+      <a
+        href="https://gheptig.github.io/react-employee-directory/"
+        target="_blank"
+      >
         <img
           class="img-fluid"
-          src="@/assets/images/baseball.png"
+          src="@/assets/images/employee.png"
           id="project-image-5"
-          alt="baseball-quiz"
+          alt="employee-directory"
         />
       </a>
     </div>
@@ -16,21 +19,23 @@
           <div
             class="project-text w-100 my-auto text-center text-lg-right mr-3"
           >
-            <h4 class="text-white">Baseball Quiz</h4>
+            <h4 class="text-white">Employee Directory</h4>
             <p class="text-white-50">
-              This is a responsive timed-quiz front-end application that
-              presents a user to answer a series of questions about baseball in
-              a given amount of time.
+              This is a simple React front-end application that allows a user to
+              filter and sort data contained in a table of employees. The
+              purpose of this application is to become familiar and demonstrate
+              the basics of React such as: writing JSX, building/importing
+              components, and setting state/using props.
             </p>
             <p class="text-white-50 mb-0">
-              Featured Technologies: jQuery, HTML, CSS, Bootstrap.
+              Featured Technologies: React.js, Node.js, Bootstrap.
             </p>
             <hr class="d-none d-lg-block mr-0" />
             <h6 class="mt-1">
               <a
                 class="text-light"
                 id="repo-link"
-                href="https://github.com/gheptig/timed-quiz"
+                href="https://github.com/gheptig/react-employee-directory"
                 target="_blank"
                 >REPO LINK</a
               >
@@ -51,7 +56,7 @@ export default {
 <style scoped>
 #project-image-5:hover {
   transform: scale(1.05);
-  box-shadow: 0px 15px 20px #5cb85c;
+  box-shadow: 0px 15px 20px #0275d8;
   border-radius: 5px;
 }
 
--- client/src/components/Projects/ProjectFour.vue
+++ client/src/components/Projects/ProjectFour.vue
@@ -2,14 +2,14 @@
   <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
     <div class="col-lg-6">
       <a
-        href="https://gheptig.github.io/react-employee-directory/"
+        href="https://powerful-everglades-55525.herokuapp.com/"
         target="_blank"
       >
         <img
           class="img-fluid"
-          src="@/assets/images/employee.png"
+          src="@/assets/images/google.png"
           id="project-image-4"
-          alt="employee-directory"
+          alt="google-book-search"
         />
       </a>
     </div>
@@ -17,23 +17,21 @@
       <div class="bg-black text-center h-100 project">
         <div class="d-flex h-100">
           <div class="project-text w-100 my-auto text-center text-lg-left ml-3">
-            <h4 class="text-white">Employee Directory</h4>
+            <h4 class="text-white">Google Book Search</h4>
             <p class="text-white-50">
-              This is a simple React front-end application that allows a user to
-              filter and sort data contained in a table of employees. The
-              purpose of this application is to become familiar and demonstrate
-              the basics of React such as: writing JSX, building/importing
-              components, and setting state/using props.
+              This is a responsive MERN stack application used to display and
+              save books using the Google Books API based on a user's search.
             </p>
             <p class="text-white-50 mb-0">
-              Featured Technologies: React.js, Node.js, Bootstrap.
+              Featured Technologies: React.js, Node.js, Express.js, MongoDB,
+              Mongoose, React-Bootstrap.
             </p>
             <hr class="d-none d-lg-block ml-0" />
             <h6 class="mt-1">
               <a
                 class="text-light"
                 id="repo-link"
-                href="https://github.com/gheptig/react-employee-directory"
+                href="https://github.com/gheptig/google-book-search"
                 target="_blank"
                 >REPO LINK</a
               >
@@ -54,7 +52,7 @@ export default {
 <style scoped>
 #project-image-4:hover {
   transform: scale(1.05);
-  box-shadow: 0px 15px 20px #0275d8;
+  box-shadow: 0px 15px 20px #b1b1b9;
   border-radius: 5px;
 }
 
